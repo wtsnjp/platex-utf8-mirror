c51ca8cdf079c944fc557e12dc77b60bc80bce25
Takuto ASAKURA <wtsnjp@gmail.com>
2021-03-14 02:57:47 +0900

    do not convert files in tests

--- mirror.sh
+++ mirror.sh
@@ -6,20 +6,24 @@ ORIGINAL_DIR='./platex-original/'
 TARGET_DIR='./platex/'
 
 # copy the directory structure
-gfind $ORIGINAL_DIR -type d -name .git -prune -o -type d | gsed 's#./platex-original/##' \
-  | ggrep -v .git | xargs -I % mkdir -p $TARGET_DIR%
+find $ORIGINAL_DIR -type d -name .git -prune -o -type d | sed 's#./platex-original/##' \
+  | grep -v .git | xargs -I % mkdir -p $TARGET_DIR%
 
 # copy binary files
-gfind $ORIGINAL_DIR -type d -name .git -prune -o -type f -name '*.pdf' -o -name '*.dvi' \
-  | ggrep -v .git | xargs -I % gcp -f % $TARGET_DIR
+find $ORIGINAL_DIR -type d -name .git -prune -o -type f -name '*.pdf' -o -name '*.dvi' \
+  | grep -v .git | xargs -I % cp -f % $TARGET_DIR
 
 # convert encoding for all text files
-gfind $ORIGINAL_DIR -type d -name .git -prune -o -type f -not -name '*.pdf' -not -name '*.dvi' \
-  | ggrep -v .git | while read line
+find $ORIGINAL_DIR -type d -name .git -prune -o -type f -not -name '*.pdf' -not -name '*.dvi' \
+  | grep -v .git | while read file
 do
-  target=$(echo $line | gsed -e "s#$ORIGINAL_DIR#$TARGET_DIR#g")
-  iconv -f ISO-2022-JP -t UTF8 $line > $target
+  target=$(echo $file | sed -e "s#$ORIGINAL_DIR#$TARGET_DIR#g")
+  if [[ $(file -i $file | grep utf-8) ]]; then
+    cp -f $file $target
+  else
+    iconv -f ISO-2022-JP -t UTF8 $file > $target
+  fi
 done
 
 # copy the .gitignore
-gcp $ORIGINAL_DIR.gitignore $TARGET_DIR.gitignore
+cp -f $ORIGINAL_DIR.gitignore $TARGET_DIR.gitignore
--- platex/tests/dstcheck.pl
+++ platex/tests/dstcheck.pl
@@ -8,4 +8,68 @@
 ##  
 ##  File: platex.dtx
 ##
-## DOCSTRIP 
+## DOCSTRIP 文書内の環境や条件の入れ子を調べる perl スクリプト
+##
+push(@dst,"DUMMY"); push(@dst,"000");
+push(@env,"DUMMY"); push(@env,"000");
+while (<>) {
+  if (/^%<\*([^>]+)>/) { # check conditions
+    push(@dst,$1);
+    push(@dst,$.);
+  } elsif (/^%<\/([^>]+)>/) {
+    $linenum = pop(@dst);
+    $conditions = pop(@dst);
+    if ($1 ne $conditions) {
+      if ($conditions eq "DUMMY") {
+        print "$ARGV: `</$1>' (l.$.) is not started.\n";
+        push(@dst,"DUMMY");
+        push(@dst,"000");
+      } else {
+        print "$ARGV: `<*$conditions>' (l.$linenum) is ended ";
+        print "by `<*$1>' (l.$.)\n";
+      }
+    }
+  }
+  if (/^% *\\begin\{verbatim\}/) { # check environments
+    while(<>) {
+        last if (/^% *\\end\{verbatim\}/);
+    }
+  } elsif (/^% *\\begin\{([^{}]+)\}\{(.*)\}/) {
+    push(@env,$1);
+    push(@env,$.);
+  } elsif (/^% *\\begin\{([^{}]+)\}/) {
+    push(@env,$1);
+    push(@env,$.);
+  } elsif (/^% *\\end\{([^{}]+)\}/) {
+    $linenum = pop(@env);
+    $environment = pop(@env);
+    if ($1 ne $environment) {
+      if ($environment eq "DUMMY") {
+        print "$ARGV: `\\end{$1}' (l.$.) is not started.\n";
+        push(@env,"DUMMY");
+        push(@env,"000");
+      } else {
+        print "$ARGV: \\begin{$environement} (l.$linenum) is ended ";
+        print "by \\end{$1} (l.$.)\n";
+      }
+    }
+  }
+}
+$linenum = pop(@dst);
+$conditions = pop(@dst);
+while ($conditions ne "DUMMY") {
+    print "$ARGV: `<*$conditions>' (l.$linenum) is not ended.\n";
+    $linenum = pop(@dst);
+    $conditions = pop(@dst);
+}
+$linenum = pop(@env);
+$environment = pop(@env);
+while ($environment ne "DUMMY") {
+    print "$ARGV: `\\begin{$environment}' (l.$linenum) is not ended.\n";
+    $linenum = pop(@env);
+    $environment = pop(@env);
+}
+exit;
+##  
+## 
+##  End of file `dstcheck.pl'.
