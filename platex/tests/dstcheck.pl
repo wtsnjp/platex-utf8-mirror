5764b69e87cb65e27ba0cd5efa810a68be281aa4
Takuto ASAKURA <wtsnjp@gmail.com>
2021-03-13 23:51:18 +0900

    first converted code from 5db1ffd

---
+++ platex/tests/dstcheck.pl
@@ -0,0 +1,11 @@
+## 
+##  This is file `dstcheck.pl',
+##  generated with the docstrip utility.
+## 
+##  The original source files were:
+## 
+##  platex.dtx  (with options: `plprog')
+##  
+##  File: platex.dtx
+##
+## DOCSTRIP 
